369a02a6e6aa2b9b52166aa1dcce3cad33a048b5
sxzhangyi <sxzhangyi@github.com>
2026-05-13 21:25:23 +0800

    fix-code-cleanup

--- lib/shared/decorators/coin_shimmer_decorator.dart
+++ lib/shared/decorators/coin_shimmer_decorator.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../core/constants/layout_constants.dart';
 
 class CoinShimmerDecorator extends StatefulWidget {
   final Widget child;
@@ -13,26 +12,16 @@ class CoinShimmerDecorator extends StatefulWidget {
 class _CoinShimmerDecoratorState extends State<CoinShimmerDecorator>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _scaleAnimation;
-  late Animation<double> _opacityAnimation;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(
-        milliseconds: 1500,
-      ),
+      duration: const Duration(milliseconds: 1500),
+      lowerBound: 0.0,
+      upperBound: 1.0,
     )..repeat(reverse: true);
-
-    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.2).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-    );
-
-    _opacityAnimation = Tween<double>(begin: 1.0, end: 0.6).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-    );
   }
 
   @override
@@ -46,10 +35,12 @@ class _CoinShimmerDecoratorState extends State<CoinShimmerDecorator>
     return AnimatedBuilder(
       animation: _controller,
       builder: (context, child) {
+        final scale = 1.0 + 0.2 * _controller.value;
+        final opacity = 1.0 - 0.4 * _controller.value;
         return Transform.scale(
-          scale: _scaleAnimation.value,
+          scale: scale,
           child: Opacity(
-            opacity: _opacityAnimation.value,
+            opacity: opacity,
             child: child,
           ),
         );
